af11a8b9b457457ef3be0415e9b10d8e97468644
Jindrich Mynarz <mynarzjindrich@gmail.com>
2015-10-18 19:04:04 +0200

    Leaner Mustache functions

--- src/db_quiz_prep/mustache.clj
+++ src/db_quiz_prep/mustache.clj
@@ -13,21 +13,3 @@
        distinct
        set))
 
-(defn validate
-  "Validate the correctness of Mustache syntax in `template`."
-  [template]
-  (try (parse template) nil
-       (catch Exception e (join-lines ["The provided Mustache template is invalid:"
-                                       (.getMessage e)]))))
-
-(defn all-variables-provided?
-  "Validates if `config` provides all variables used in `template`."
-  [config template]
-  (let [map-set-names (comp set (partial map name))
-        template-variables (map-set-names (difference (get-template-variables template) #{:limit :offset}))
-        provided-variables (map-set-names (keys (get config :params {})))
-        missing-variables (difference template-variables provided-variables)]
-    (when-let [error (or (nil? (seq template-variables))
-                         (seq missing-variables))]
-      (join-lines ["Configuration is missing these variables used in the template:"
-                   (string/join ", " missing-variables)]))))
