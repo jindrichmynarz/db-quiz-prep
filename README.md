f5148a4f51bd03afb0a2158c191e69d45f58b649
Jindrich Mynarz <mynarzjindrich@gmail.com>
2015-10-18 17:23:17 +0200

    Housekeeping

---
+++ README.md
@@ -0,0 +1,13 @@
+# db-quiz-prep
+
+A tool for pre-processing data for [DB-quiz](https://github.com/jindrichmynarz/db-quiz).
+
+## Usage
+
+Run the compiled JAR-file with the `--help` switch.
+
+## License
+
+Copyright © 2015 Jindřich Mynarz
+
+Distributed under the Eclipse Public License version 1.0.
--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject db-quiz-prep "0.1.0-SNAPSHOT"
+(defproject db-quiz-prep "0.2.0-SNAPSHOT"
   :description "Data pre-processing for DB-quiz"
-  :url "http://example.com/FIXME"
+  :url "http://github.com/jindrichmynarz/db-quiz-prep"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
